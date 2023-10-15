5cfbc2fa57b0a5588b5e9d9a3b6d58b3d36883e6
MyManny <gmail.com>
2023-10-14 21:02:10 -0700

    updated components

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import TopBar from './Components/NavBar';
+import Card1 from './Components/Card';
+import Store from './Components/StoreListing';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <TopBar />
+    <Card1 />
+    <Store />
   </React.StrictMode>,
   document.getElementById('root')
 );
